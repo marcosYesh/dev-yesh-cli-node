17801af2e31e4b1b3f09e74ee0466f8078600ca9
Marcão <marcos@yesh.com.br>
2024-05-16 17:41:12 -0300

    configuração de definição do caminho usado para gração de arquivos e pastas

--- src/utils/fileUtils.js
+++ src/utils/fileUtils.js
@@ -1,4 +1,3 @@
-// fileUtils.js
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -7,7 +6,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export function createFolders(folders) {
     Object.values(folders).forEach(folder => {
-        const folderPath = path.join(__dirname, '..', folder);
+        const folderPath = path.join(__dirname, '..', '..', '..', '..', 'src', folder); // Voltando cinco pastas
         if (!fs.existsSync(folderPath)) {
             fs.mkdirSync(folderPath, { recursive: true });
             console.log(`Created folder: ${folder}`);
@@ -19,7 +18,7 @@ export function createFolders(folders) {
 
 export function createFiles(files) {
     files.forEach(file => {
-        const filePath = path.join(__dirname, '..', file);
+        const filePath = path.join(__dirname, '..', '..', '..', '..', 'src', file); // Voltando cinco pastas
         if (!fs.existsSync(filePath)) {
             fs.writeFileSync(filePath, '', { flag: 'wx' });
             console.log(`Created file: ${file}`);
